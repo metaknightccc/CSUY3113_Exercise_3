4858f86a794d4fd1b243a3f7418826db0db14d40
metaknightccc <sunyifanc@126.com>
2022-09-30 04:10:18 -0400

    Complete the wave manager and add the end scene

--- Assets/Code/LevelControl.cs
+++ Assets/Code/LevelControl.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using Update = Unity.VisualScripting.Update;
 
 public class LevelControl : MonoBehaviour
 {
-
-    public Spawner spawner;
-    // Start is called before the first frame update
+    [SerializeField] private Wave wave;
     void Start()
     {
+        StartBattle();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartBattle(){
+        wave.SpawnEnemies(1);
+    }
+    
+    public void Update()
     {
-        spawner.stopSpawning = !spawner.stopSpawning;
+        wave.CheckAlive();
+    }
+    
+    [System.Serializable]
+    private class Wave
+    {
+        private int wave_num = 1;
+        public void CheckAlive()
+        {
+            foreach (EnemyController i in Spawner.GetInstance().AliveList)
+            {
+                if (i.isAlive == true) return;
+            }
+            SpawnEnemies(++wave_num);
+        }
+        
+        public void SpawnEnemies(int wave_num)
+        {
+            Spawner.GetInstance().StartSpawn(wave_num);
+        }
     }
 }
--- Assets/Code/Spawner.cs
+++ Assets/Code/Spawner.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Spawner : MonoBehaviour
+public class Spawner : Singleton<Spawner>
 {
     public Transform[] SpawnLocations;
     public GameObject[] SpawnPrefab;
-    
+    public List<EnemyController> AliveList;
 
-    public bool stopSpawning = false;
-    public float spawnTime;
-    public float spawnDelay;
-
-    void Start(){
-        InvokeRepeating("StartSpawn", spawnTime, spawnDelay);
-    }
-
-    void StartSpawn(){
-        Instantiate(SpawnPrefab[0], SpawnLocations[0].transform.position, transform.rotation);
-        Instantiate(SpawnPrefab[0], SpawnLocations[1].transform.position, transform.rotation);
-        Instantiate(SpawnPrefab[0], SpawnLocations[2].transform.position, transform.rotation);
-        if(stopSpawning){
-            CancelInvoke("StartSpawn");
+    public void StartSpawn(int wave){
+        AliveList.Clear();
+        if (wave == 1)
+        {
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[0].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[1].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
         }
+        if (wave == 2)
+        {
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[0].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[1].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[1], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[1], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+        }
+        if (wave == 3)
+        {
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[0].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[1].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[1], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[1], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+            AliveList.Add(Instantiate(SpawnPrefab[2], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
+        }
+        if (wave == 4)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+        
     }
 }
