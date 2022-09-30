28bb04377856e5fa3e980e5dcdbd6c4e14de0603
Sabahat-Sami <70985974+Sabahat-Sami@users.noreply.github.com>
2022-09-30 12:36:02 -0400

    Adjusted spawner so enemies didn't spawn on roofs and added sounds and improved navmesh so enemies didn't phase through walls

--- Assets/Code/Spawner.cs
+++ Assets/Code/Spawner.cs
@@ -21,6 +21,7 @@ public class Spawner : Singleton<Spawner>
         if (wave == 1)
         {
             waveText.text = wave + "/3 waves";
+            _audioSource.PlayOneShot(startWaveSound1);
             AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[0].transform.position, transform.rotation).GetComponent<EnemyController>());
             AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[1].transform.position, transform.rotation).GetComponent<EnemyController>());
             AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[2].transform.position, transform.rotation).GetComponent<EnemyController>());
@@ -36,6 +37,7 @@ public class Spawner : Singleton<Spawner>
         }
         if (wave == 3)
         {
+            _audioSource.PlayOneShot(startWaveSound3);
             waveText.text = wave + "/3 waves";
             AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[0].transform.position, transform.rotation).GetComponent<EnemyController>());
             AliveList.Add(Instantiate(SpawnPrefab[0], SpawnLocations[1].transform.position, transform.rotation).GetComponent<EnemyController>());
